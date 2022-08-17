e2bf48f567eb2f8c5d2aa539c0d1d826a4c65828
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2022-08-17 16:00:32 -0700

    Setup

---
+++ src/main/java/net/mmmjava/meeps/Meeps.java
@@ -0,0 +1,12 @@
+package net.mmmjava.meeps;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Meeps.MOD_ID)
+public class Meeps {
+    public static final MOD_ID = "meeps";
+
+    public Meeps() {
+        IEvent
+    }
+}
